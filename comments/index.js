45221806eba561f9b79c720b55ef6b647d39d49e
Tudor Hoinaru <tudor.hoinaru@gmail.com>
2020-04-29 21:44:25 +0300

    Added comment state

--- comments/index.js
+++ comments/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-
+const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,15 +13,49 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status: 'pending'
+        }
+    });
+
     res.status(201).send(commentsByPostId[req.params.id]);
+});
+
+app.post('/events', async (req, res) => {
+    console.log('Received Event:', req.body.type);
+    const { type, data } = req.body;
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(c => {
+            return c.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                content,
+                postId,
+                status
+            }
+        });
+    }
+    res.send({});
 });
 
 app.listen(4001, () => {
